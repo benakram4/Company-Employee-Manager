90dcefae7fdc0e5a39b0fd717eb4346b3ed4f523
benakram4 <105305419+benakram4@users.noreply.github.com>
2022-10-04 00:13:11 -0400

    1.0

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var data = require("./data-service.js");
 
 var HTTP_PORT = process.env.PORT || 8080;  // || : or
 
+app.use(express.static('public'));
+
 // call this function after the http server starts listening for requests
 function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
 }
-
-app.use(express.static('public'));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/views/home.html"))
@@ -22,39 +22,25 @@ app.get("/about", function(req, res){
     
 });
 
-// app.get("/employees", function(req, res){
-//     var text = "<h3>TODO emp</h3>"
-//     res.send(text);
-// });
-
-// app.get("/managers", function(req, res){
-//     var text = "<h3>TODO man</h3>"
-//     res.send(text);
-// });
-
-// app.get("/departments", function(req, res){
-//     var text = "<h3>TODO dept</h3>"
-//     res.send(text);
-// });
-
 app.get("/employees", function(req, res){
     data.getAllEmployees()
-    .then((data) => res.json(data))
-    .catch((err) => res.json(`${err}`));
+    .then((data) => {res.json(data)})
+    .catch((err) => {console.log(`${err}`)});
     console.log("getting emp");
 });
 
 app.get("/managers", function(req, res){
     data.getManagers()
-    .then((data) => res.json(data))
-    .catch((err) => res.json(`${err}`));
+    .then((data) => {res.json(data)})
+    .catch((err) => {console.log(`${err}`)});
     console.log("getting managers");
 });
 
+
 app.get("/departments", function(req, res){
     data.getDepartments()
-    .then((data) => res.json(data))
-    .catch((err) => res.json(`${err}`));
+    .then((data) => {res.json(data)})
+    .catch((err) => {console.log(`${err}`)});
     console.log("getting dep");
 });
 
