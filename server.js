b948496778912c734a9fbe3d9307453beeac28b2
benakram4 <105305419+benakram4@users.noreply.github.com>
2022-10-05 21:05:33 -0400

    added declaration

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/********************************************************************************* 
+ *  BTI325 – Assignment 2 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+ *  No part of this assignment has been copied manually or electronically from any other source 
+ *  (including web sites) or distributed to other students.
+ * 
+ *  Name: Ben Akram Student ID: 158523217 Date: 5/10/2022 *
+ *  Online (Cyclic) URL:  
+ * *  https://glamorous-goat-slacks.cyclic.app/ *
+  ********************************************************************************/  
+
 var express = require("express"); // Include express.js module
 var app = express();
 
