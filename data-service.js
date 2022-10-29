5e90179e15825ec73fa2739cb930b092641ecdeb
benakram4 <105305419+benakram4@users.noreply.github.com>
2022-10-28 22:43:00 -0400

    A3

--- data-service.js
+++ data-service.js
@@ -13,7 +13,7 @@ exports.initialize = () => {
             employees = JSON.parse(data);
             fs.readFile("./data/departments.json", (err, data) =>{
                 if(err){
-                    reject("Error when reading deparments data");
+                    reject("Error when reading departments data");
                 }
                 else{
                     departments = JSON.parse(data);
@@ -48,13 +48,99 @@ exports.getManagers = () =>{
         if(employees.length == 0){
             reject("No Employees in the Data");
         }
-
-        for(let m in employees){
-            //console.log("lalal");
-            if(employees[m].isManager == true){
-                managers.push(employees[m]);
+        else{
+            for(let m in employees){
+                if(employees[m].isManager == true){
+                    managers.push(employees[m]);
+                }
             }
         }
         resolve(managers);
+    });
+}
+
+//addEmployee function
+exports.addEmployee = (employeeData) => {
+    return new Promise((resolve, reject) => {
+        employeeData.isManager = (employeeData.isManager === undefined) ? true : false;
+        employeeData.employeeNum = employees.length + 1;
+        employees.push(employeeData);
+        resolve();
+    });
+}
+
+//getEmployeesByStatus function
+exports.getEmployeesByStatus = (status) => {    
+    return new Promise((resolve, reject) => {
+        // temp object to store the employees with the status
+        let i = 0;
+        var empStatus = []; 
+        for(const property in employees){
+            //console.log("Searching for employee status: " + status);
+            //console.log("Employee status: " + employees[property].status);
+            if(employees[property].status == status){
+
+                //console.log(`Found ${i + 1} Employee with that status`);
+                i++;
+                empStatus.push(employees[property]);
+            }
+        }
+        if(empStatus.length == 0 ){
+            reject("No Employees with that status");
+        }
+        resolve(empStatus);
+    });
+}
+
+//getEmployeesByDepartment function
+exports.getEmployeesByDepartment = (department) => {   
+    return new Promise((resolve, reject) => {
+        // temp object to store the employees with the department
+        var empDepartment = []; 
+        for(const property in employees){
+            if(employees[property].department == department){
+                empDepartment.push(employees[property]);
+            }
+        }
+        if(empDepartment.length == 0){
+            reject("No Employees with that department");
+        }
+        resolve(empDepartment);
+    });
+};
+
+//getEmployeesByManager function
+exports.getEmployeesByManager = (managerNum) => {
+    return new Promise((resolve, reject) => {
+        // temp object to store the employees with the manager
+        var empManager = []; 
+        for(const property in employees){
+            if(employees[property].employeeManagerNum == managerNum){
+                console.log("Found Employees with that manager");
+                empManager.push(employees[property]);
+            }
+        }
+        if(empManager.length == 0){
+            reject("No Employees with that manager");
+        }
+        resolve(empManager);
+    });
+};
+
+//getEmployeeByNum function
+exports.getEmployeeByNum = (num) => {
+    return new Promise((resolve, reject) => {
+        // temp object to store the employees with the number
+        var empNum;
+        console.log("Searching for employee number: " + num);
+        for(const property in employees){
+            if(employees[property].employeeNum == num){
+               empNum = employees[property]; 
+        }
+    }
+        if(empNum == null || empNum == undefined){
+            reject("No Employees with that number");
+        }
+        resolve(empNum);
     });
 }
--- server.js
+++ server.js
@@ -10,53 +10,150 @@
 
 var express = require("express"); // Include express.js module
 var app = express();
+app.use(express.json()); // parse incoming JSON requests and place them in req.body
+app.use(express.urlencoded({ extended: true })); // parse incoming urlencoded form data and place it in req.body
 
-var path = require("path"); // include moduel path to use __dirname, and function path.join()
+var path = require("path"); // include module path to use __dirname, and function path.join()
 var data = require("./data-service.js");
 
-var HTTP_PORT = process.env.PORT || 8080;
+var multer = require("multer"); // include file handling module
+var fs = require("fs");  //includes writing to a file 
 
 app.use(express.static('public'));
 
+var HTTP_PORT = process.env.PORT || 8080;
 // call this function after the http server starts listening for requests
 function onHttpStart(){
     console.log("Express http server listening on port: " + HTTP_PORT);
-}
+};
 
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+//home route
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
+//about page route
 app.get("/about", function(req, res){
     res.sendFile(path.join(__dirname, "/views/about.html"))
     
 });
 
-app.get("/employees", function(req, res){
-    data.getAllEmployees()
-    .then((data) => {res.json(data)})
-    .catch((err) => {console.log(`${err}`)});
-    console.log("getting emp");
+// images route
+app.get("/images", function(req, res){
+    //return all images in the images folder
+    fs.readdir("./public/images/uploaded", (err, images) =>{
+        if(err){
+            console.log("Error reading images");
+        }
+        else{
+            res.json(images);
+        }
+    });
 });
 
+
+//employees/add route
+app.get("/employees/add", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/addEmployee.html"))
+});
+
+//images/add route
+app.get("/images/add", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+});
+
+
+//employees route and employee related queries
+app.get("/employees", function(req, res){
+
+    if(req.query.status){ //query with employees status
+        console.log("this is req.query.status: " + req.query.status);
+        data.getEmployeesByStatus(req.query.status)
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))
+        console.log("getting emp : getEmployeesByStatus()");
+    }   
+    else if(req.query.department){ //query with employees department
+        console.log("this is req.query.department: " + req.query.department);
+        data.getEmployeesByDepartment(req.query.department)
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))
+    }
+    else if(req.query.manager){ //query with employees manager id
+        console.log("this is req.query.manager: " + req.query.manager);
+        data.getEmployeesByManager(req.query.manager)
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))
+    }
+    else{
+        data.getAllEmployees() // without query 
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))
+        console.log("getting all employees : getAllEmployees()");
+    }
+});
+
+//managers route
 app.get("/managers", function(req, res){
     data.getManagers()
     .then((data) => {res.json(data)})
     .catch((err) => {console.log(`${err}`)});
-    console.log("getting managers");
+    console.log("getting managers : getManagers()");
 });
 
-
+//departments route
 app.get("/departments", function(req, res){
     data.getDepartments()
     .then((data) => {res.json(data)})
     .catch((err) => {console.log(`${err}`)});
-    console.log("getting dep");
+    console.log("getting dep : getDepartments()");
 });
+
+//upload image function
+app.post("/images/add",upload.single("imageFile") ,function(req, res){
+    fs.readdir("./public/images/uploaded", (err, files) => {
+        console.log("Uploading Image");
+        if(err){
+            console.log(err);
+            console.log("Failed to upload Image");
+        }
+        else{
+            console.log("Image Uploaded successfully");
+            res.json({images:files});
+        }
+     });
+     
+});
+
+//add employee function
+app.post("/employees/add",(req,res) =>{
+    data.addEmployee(req.body)
+    .then(() => {res.redirect("/employees")})
+    .catch((err) => {console.log(`Error adding emp${err}`)});
+    console.log("adding emp");
+})
+
+//get employee by emp number function
+app.get("/employee/:value", (req, res) =>{
+    data.getEmployeeByNum(req.params.value)
+    .then((data) => {res.json(data)})
+    .catch((err) => {console.log(`Error getting employees by value: ${err}`)});
+    console.log("getting employees by value : getEmployeeByNum()");
+    
+})
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "views/error404.html"));
-  });
+});
   
 data.initialize().then(app.listen(HTTP_PORT, onHttpStart));
 //app.listen(HTTP_PORT, onHttpStart());
